b38e25408fb23054428092b0143fe6a95f57a3d5
Pawel.Piasecki <250988@edu.p.lodz.pl>
2024-05-23 18:57:11 +0200

    added option to enter text and changing size of letters

---
+++ text.h
@@ -0,0 +1,19 @@
+//
+// Created by pawel on 23.05.2024.
+//
+
+#ifndef CHANGING_LETTERS_TEXT_H
+#define CHANGING_LETTERS_TEXT_H
+void enterText (char *text);
+
+void printText(char *text);
+
+int textLenght(char *text);
+
+void clear(void);
+
+void lowerCase(char *text);
+
+void upperCase (char *text);
+
+#endif //CHANGING_LETTERS_TEXT_H
